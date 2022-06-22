78ce26d83216dba2b7f6d47aa0c32d1701495a10
brokerr1 <solovievdima1@gmail.com>
2022-06-22 12:49:49 +0300

    8

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Методом прямоугольника вычеслить интеграл от 0 до 1,2 (sin(-x^2 + 2)dx)/(2- cos(x^2)) с шагом 0.01
+double result = IntegralPr(0, 1.2, 0.01);
+Console.WriteLine($"Результат: {result}");
+double IntegralPr(double a, double b, double h)
+{
+	double s = 0;
+	for (double i = a; i < b - h; i += h)
+    {
+		s = s + (f(i) + f(i + h)) / 2 * h;
+	}
+	return s;
+}
+double f(double x)
+{
+	return Math.Pow(x, 2);
+}
